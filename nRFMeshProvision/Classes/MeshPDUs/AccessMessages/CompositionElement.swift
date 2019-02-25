8d4df2eaa377af6ecb082afe7dc837764e06d998
Dominique <domi.github@gmail.com>
2019-02-25 14:35:06 +0100

    feat(node): update node serialization

--- nRFMeshProvision/Classes/MeshPDUs/AccessMessages/CompositionElement.swift
+++ nRFMeshProvision/Classes/MeshPDUs/AccessMessages/CompositionElement.swift
@@ -8,40 +8,41 @@
 import Foundation
 
 public struct CompositionElement: Codable {
-
     // MARK: - Properties
+
     var index: Int?
-    var location                    : Data
-    var sigModelCount               : Int
-    var vendorModelCount            : Int
-    var sigModels                   : [Data]
-    var vendorModels                : [Data]
-    var allModels                   : [Data]
-    var modelKeyBindings            : [Data: Data]
-    var modelPublishAddress         : [Data: Data]
-    var modelSubscriptionAddresses  : [Data: [Data]]
+    var location: Data
+    var sigModelCount: Int
+    var vendorModelCount: Int
+    var sigModels: [Data]
+    var vendorModels: [Data]
+    var allModels: [Data]
+    var modelKeyBindings: [Data: Data]
+    var modelPublishAddress: [Data: Data]
+    var modelSubscriptionAddresses: [Data: [Data]]
 
     // MARK: - Initialization
+
     init(withData data: Data) {
-        modelKeyBindings            = [Data: Data]()
-        modelPublishAddress         = [Data: Data]()
-        modelSubscriptionAddresses  = [Data: [Data]]()
+        modelKeyBindings = [Data: Data]()
+        modelPublishAddress = [Data: Data]()
+        modelSubscriptionAddresses = [Data: [Data]]()
         location = Data([data[1], data[0]])
         sigModelCount = Int(data[2])
         vendorModelCount = Int(data[3])
         sigModels = [Data]()
         vendorModels = [Data]()
-        for aSigModelIndex in 0..<sigModelCount {
+        for aSigModelIndex in 0 ..< sigModelCount {
             sigModels.append(Data([data[(2 * aSigModelIndex) + 5], data[(2 * aSigModelIndex) + 4]]))
         }
-        for aVendorModelIndex in 0..<vendorModelCount {
+        for aVendorModelIndex in 0 ..< vendorModelCount {
             vendorModels.append(
                 Data([
                     data[(2 * sigModelCount) + (4 * aVendorModelIndex) + 5],
                     data[(2 * sigModelCount) + (4 * aVendorModelIndex) + 4],
                     data[(2 * sigModelCount) + (4 * aVendorModelIndex) + 7],
-                    data[(2 * sigModelCount) + (4 * aVendorModelIndex) + 6]
-                    ])
+                    data[(2 * sigModelCount) + (4 * aVendorModelIndex) + 6],
+                ])
             )
         }
         allModels = [Data]()
@@ -50,6 +51,7 @@ public struct CompositionElement: Codable {
     }
 
     // MARK: - Accessors
+
     public func publishAddressForModelId(_ aModelId: Data) -> Data? {
         return modelPublishAddress[aModelId]
     }
@@ -84,21 +86,13 @@ public struct CompositionElement: Codable {
         }
     }
 
-    public mutating func removeKeyBinding(_ aKey: Data, forModelId aModelId: Data) {
-        if let modelIndex = modelKeyBindings.index(forKey: aModelId) {
-            modelKeyBindings.remove(at: modelIndex)
-        }
+    public mutating func removeKeyBinding(_: Data, forModelId aModelId: Data) {
+        modelKeyBindings.removeValue(forKey: aModelId)
     }
+
     public mutating func setKeyBinding(_ aKey: Data, forModelId aModelId: Data) {
-        if modelKeyBindings[aModelId] == nil {
-            modelKeyBindings[aModelId] = aKey
-        } else {
-            if let modelIndex = modelKeyBindings.index(forKey: aModelId) {
-                //Replace old value with newer one
-                modelKeyBindings.remove(at: modelIndex)
-                modelKeyBindings[aModelId] = aKey
-            }
-        }
+        modelKeyBindings.removeValue(forKey: aModelId)
+        modelKeyBindings[aModelId] = aKey
     }
 
     public func allSigAndVendorModels() -> [Data] {
@@ -118,19 +112,72 @@ public struct CompositionElement: Codable {
     }
 
     public func elementLocation() -> Data {
-        return location;
+        return location
     }
 
     enum CodingKeys: String, CodingKey {
         case index
         case location
-        case sigModelCount
-        case vendorModelCount
-        case sigModels
-        case vendorModels
         case allModels = "models"
-        case modelKeyBindings
-        case modelPublishAddress
-        case modelSubscriptionAddresses
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        index = try values.decode(Int.self, forKey: .index)
+        let locationString = try values.decode(String.self, forKey: .location)
+        location = Data(hexString: locationString) ?? Data([0x00, 0x00]);
+        let models = try values.decode([MeshModel].self, forKey: .allModels);
+        sigModels = [Data]()
+        vendorModels = [Data]();
+        modelKeyBindings = [Data: Data]()
+        modelPublishAddress = [Data: Data]()
+        modelSubscriptionAddresses = [Data: [Data]]()
+        sigModelCount = 0
+        vendorModelCount = 0
+        allModels = [Data]();
+        models.forEach { (model) in
+            if let modelId = Data(hexString: model.modelId) {
+                if (model.modelId.count == 8) {
+                    vendorModels.append(modelId)
+                } else {
+                    sigModels.append(modelId)
+                }
+                // LATER: move to list of key bindings (not using only the first one)
+                if let bind = model.bind {
+                    if let firstBind = bind.first {
+                        if let firstBindData = Data(hexString: firstBind) {
+                            modelKeyBindings[modelId] = firstBindData
+                        }
+                    }
+                }
+            }
+        }
+        allModels.append(contentsOf: sigModels)
+        allModels.append(contentsOf: vendorModels)
+        sigModelCount = sigModels.count
+        vendorModelCount = vendorModels.count
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(index, forKey: .index)
+        try container.encode(location.hexString(), forKey: .location)
+        //try container.encode(allModels, forKey: .allModels)
+        var models = [MeshModel]()
+        allModels.forEach { (modelId) in
+            models.append(MeshModel(modelId: modelId.hexString(), bind: modelKeyBindings[modelId] != nil ? [modelKeyBindings[modelId]!.hexString()] : nil))
+        }
+        try container.encode(models, forKey: .allModels)
+    }
+}
+
+public struct MeshModel: Codable {
+    var modelId: String
+    var bind: [String]?
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(modelId, forKey: .modelId)
+        try container.encodeIfPresent(bind, forKey: .bind)
     }
 }
--- nRFMeshProvision/Classes/MeshState/MeshNodeEntry.swift
+++ nRFMeshProvision/Classes/MeshState/MeshNodeEntry.swift
@@ -8,43 +8,47 @@
 import Foundation
 
 public class MeshNodeEntry: NSObject, Codable {
-
     // MARK: - Properties
-    public var UUID                    : String?
-    public let nodeName                : String
-    public let provisionedTimeStamp    : Date
-    public let nodeId                  : Data
-    public let deviceKey               : Data
-    public var appKeys                 : [Data]
-    public var nodeUnicast             : Data?
+
+    public var UUID: String?
+    public let nodeName: String // TODO: investigate why correct name is not set
+    public let provisionedTimeStamp: Date
+    public let nodeId: Data
+    public let deviceKey: Data
+    public var appKeys: [Data]
+    public var nodeUnicast: Data?
 
     // MARK: - Todo
+
     public var security = "low"
     public var netKeys: [Data]?
-    public var configComplete: Bool?
-    public var ttl: Data?
+    public var configComplete = false
+    public var ttl = Data([0x00, 0x05])
     public var blacklisted: Bool?
 
     // MARK: -  Node composition
-    public var companyIdentifier       : Data?
-    public var productIdentifier       : Data?
-    public var productVersion          : Data?
-    public var replayProtectionCount   : Data?
-    public var featureFlags            : Data?
-    public var elements                : [CompositionElement]?
+
+    public var companyIdentifier: Data?
+    public var productIdentifier: Data?
+    public var productVersion: Data?
+    public var replayProtectionCount: Data?
+    public var featureFlags: Data?
+    public var elements: [CompositionElement]?
 
     // MARK: - Initialization
+
     public init(withName aName: String, provisionDate aProvisioningTimestamp: Date, nodeId anId: Data, andDeviceKey aDeviceKey: Data) {
-        nodeName                    = aName
-        provisionedTimeStamp        = aProvisioningTimestamp
-        nodeId                      = anId
-        deviceKey                   = aDeviceKey
-        appKeys                     = [Data]()
+        nodeName = aName
+        provisionedTimeStamp = aProvisioningTimestamp
+        nodeId = anId
+        deviceKey = aDeviceKey
+        appKeys = [Data]()
     }
 
-     enum CodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case UUID
         case deviceKey
+        case security
         case nodeUnicast = "unicastAddress"
 
         case nodeName = "name"
@@ -62,5 +66,56 @@ public class MeshNodeEntry: NSObject, Codable {
         case configComplete
         case ttl
         case blacklisted
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        UUID = try values.decode(String.self, forKey: .UUID)
+        let deviceKeyString = try values.decode(String.self, forKey: .deviceKey)
+        deviceKey = Data(hexString: deviceKeyString) ?? OpenSSLHelper().generateRandom()
+        let nodeUnicastString = try values.decode(String.self, forKey: .nodeUnicast)
+        nodeUnicast = Data(hexString: nodeUnicastString) ?? Data([0x00, 0x00])
+        nodeName = try values.decode(String.self, forKey: .nodeName)
+        let companyIdentifierString = try values.decode(String.self, forKey: .companyIdentifier)
+        companyIdentifier = Data(hexString: companyIdentifierString)
+        let productIdentifierString = try values.decode(String.self, forKey: .productIdentifier)
+        productIdentifier = Data(hexString: productIdentifierString)
+        let productVersionString = try values.decode(String.self, forKey: .productVersion)
+        productVersion = Data(hexString: productVersionString)
+        provisionedTimeStamp = try values.decode(Date.self, forKey: .provisionedTimeStamp) // TODO: use android ait
+        nodeId = try values.decode(Data.self, forKey: .nodeId)
+        appKeys = try values.decode([Data].self, forKey: .appKeys)
+        let replayProtectionCountString = try values.decode(String.self, forKey: .replayProtectionCount)
+        replayProtectionCount = Data(hexString: replayProtectionCountString)
+        featureFlags = try values.decode(Data.self, forKey: .featureFlags) // TODO: use android ait
+        elements = try values.decode([CompositionElement].self, forKey: .elements)
+        netKeys = try values.decode([Data].self, forKey: .netKeys)
+        configComplete = try values.decode(Bool.self, forKey: .configComplete)
+        let ttlInt = try values.decode(UInt16.self, forKey: .ttl)
+        ttl = Data(fromInt16: ttlInt);
+        blacklisted = try values.decode(Bool.self, forKey: .blacklisted)
+        security = try values.decode(String.self, forKey: .security)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(UUID, forKey: .UUID)
+        try container.encode(deviceKey.hexString(), forKey: .deviceKey)
+        try container.encode(nodeUnicast, forKey: .nodeUnicast)
+        try container.encode(nodeName, forKey: .nodeName)
+        try container.encode(companyIdentifier?.hexString(), forKey: .companyIdentifier)
+        try container.encode(productIdentifier?.hexString(), forKey: .productIdentifier)
+        try container.encode(productVersion?.hexString(), forKey: .productVersion)
+        try container.encode(provisionedTimeStamp, forKey: .provisionedTimeStamp)
+        try container.encode(nodeId, forKey: .nodeId)
+        try container.encode(appKeys, forKey: .appKeys)
+        try container.encode(replayProtectionCount?.hexString(), forKey: .replayProtectionCount)
+        try container.encode(featureFlags, forKey: .featureFlags)
+        try container.encode(elements, forKey: .elements)
+        try container.encode(netKeys, forKey: .netKeys)
+        try container.encode(configComplete, forKey: .configComplete)
+        try container.encode(ttl.uint16, forKey: .ttl)
+        try container.encode(blacklisted, forKey: .blacklisted)
+        try container.encode(security, forKey: .security)
     }
 }
